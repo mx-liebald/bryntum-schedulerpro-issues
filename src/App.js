9dd889efad4890e37d56049e93ca7d505c45cd30
Bernhard Jahn <bernhard.jahn@mobilexag.de>
2021-10-22 10:04:29 +0200

    initial sandbox with empty scheduler

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/**
+ * The React App file
+ */
+
+// React libraries
+import React from 'react';
+
+// Stylings
+import './App.scss';
+
+// Application components
+import {
+    BryntumSchedulerPro,
+} from '@bryntum/schedulerpro-react';
+import { schedulerConfig } from './AppConfig';
+
+const App = () => {
+    return <BryntumSchedulerPro {...schedulerConfig} />;
+};
+
+export default App;
---
+++ src/AppConfig.js
@@ -0,0 +1,54 @@
+/**
+ * Application configuration
+ */
+import {
+    ProjectModel,
+    StringHelper
+} from '@bryntum/schedulerpro/schedulerpro.umd';
+
+const project = new ProjectModel({
+    autoLoad: true,
+});
+
+const schedulerConfig = {
+    project,
+    startDate: new Date(2020, 10, 29),
+    endDate: new Date(2021, 0, 10),
+    rowHeight: 50,
+    barMargin: 2,
+
+    viewPreset: 'weekAndDay',
+    resourceImagePath: 'users/',
+
+    columns: [
+        {
+            text: 'Resource',
+            field: 'name',
+            width: 120,
+            type: 'resourceInfo'
+        },
+        {
+            text: 'Tasks',
+            field: 'events.length',
+            width: 70,
+            align: 'right',
+            editor: false
+        }
+    ],
+
+    eventRenderer({ eventRecord: task, renderData }) {
+        if (task.showInTimeline) {
+            renderData.eventColor = 'green';
+        } else {
+            renderData.eventColor = 'blue';
+        }
+
+        return StringHelper.encodeHtml(task.name);
+    }
+};
+
+const timelineConfig = {
+    project
+};
+
+export { schedulerConfig, timelineConfig };
